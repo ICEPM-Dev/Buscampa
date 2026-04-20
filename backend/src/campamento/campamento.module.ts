3686c0d856387d5eca2d2480e7d845346f00bb38
macasteglione <matiascasteglione2002@gmail.com>
2026-04-20 15:14:42 -0300

    vercel backend fix

--- backend/src/campamento/campamento.module.ts
+++ backend/src/campamento/campamento.module.ts
@@ -7,7 +7,7 @@ import { Module } from '@nestjs/common';
 import { CampamentoService } from './campamento.service';
 import { CampamentoController } from './campamento.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { InscriptionModule } from 'src/inscription/inscription.module';
+import { InscriptionModule } from '../../src/inscription/inscription.module';
 
 @Module({
   imports: [PrismaModule, InscriptionModule], // Dependencias necesarias
