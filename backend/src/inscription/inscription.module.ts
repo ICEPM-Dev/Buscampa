40ea33986b0142a0900fea669a9d79638373d04c
macasteglione <matiascasteglione2002@gmail.com>
2026-06-10 19:39:12 -0300

    arreglo de verificacion de email y skip de inscripcion

--- backend/src/inscription/inscription.module.ts
+++ backend/src/inscription/inscription.module.ts
@@ -6,7 +6,7 @@ import { Module } from '@nestjs/common';
 import { InscriptionService } from './inscription.service';
 import { InscriptionController } from './inscription.controller';
 import { PrismaModule } from '../prisma/prisma.module';
-import { EmailModule } from 'src/email/email.module';
+import { EmailModule } from '../email/email.module';
 
 @Module({
   imports: [PrismaModule, EmailModule], // Para acceso a base de datos
